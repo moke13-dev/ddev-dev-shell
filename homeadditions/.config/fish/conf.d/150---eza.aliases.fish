cc76de28cdd86e06bc2b0954d916d5b30e8c49ce
Kemal Cokic <kemal_cokic@gmx.de>
2025-07-21 12:25:21 +0200

    feat(eza): add Dockerfile for eza and fish aliases
    - Adds Dockerfile.250---eza for eza installation
    - Adds eza.aliases.fish for fish shell aliases

---
+++ homeadditions/.config/fish/conf.d/150---eza.aliases.fish
@@ -0,0 +1,10 @@
+#!/usr/bin/fish
+# ddev-generated
+
+alias sl='eza'
+alias ls='eza'
+alias la='eza -AF --group-directories-first'
+alias ll='eza -lAhog --time-style long-iso --group-directories-first'
+alias l='eza -A --group-directories-first'
+alias l1='eza -1A --group-directories-first'
+alias lf='eza -AF --group-directories-first'
