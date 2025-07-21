0eb01f8367cb16701674d18103e18aac1ab78a33
Kemal Cokic <kemal_cokic@gmx.de>
2025-07-21 12:28:28 +0200

    feat(zoxide): add Dockerfile for zoxide and fish plugin config
    - Adds Dockerfile.250---zoxide for zoxide installation
    - Adds zoxide.plugin.fish for fish shell integration

---
+++ homeadditions/.config/fish/conf.d/250---zoxide.plugin.fish
@@ -0,0 +1,14 @@
+#!/usr/bin/fish
+# ddev-generated
+
+if command -v zoxide > /dev/null
+    # set environment variable _ZO_DATA_DIR
+    set -gx _ZO_DATA_DIR "/mnt/ddev-global-cache/$HOSTNAME/zoxide"
+
+    # create the directory if it doesn't exist
+    if not test -d "$_ZO_DATA_DIR"
+        mkdir -p "$_ZO_DATA_DIR"
+    end
+
+    zoxide init fish | source
+end
