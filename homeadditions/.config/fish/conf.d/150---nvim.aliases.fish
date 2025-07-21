f14aca11261d6d5ce2ef9a7636ce1a271ab08781
Kemal Cokic <kemal_cokic@gmx.de>
2025-07-21 12:23:20 +0200

    feat(neovim): add Dockerfile for neovim and fish aliases
    - Adds Dockerfile.200---neovim for installing neovim in the dev shell
    - Adds fish shell aliases for nvim commands

---
+++ homeadditions/.config/fish/conf.d/150---nvim.aliases.fish
@@ -0,0 +1,15 @@
+#!/usr/bin/fish
+# ddev-generated
+
+alias vi='nvim'
+alias Vi='nvim'
+alias vI='nvim'
+alias VI='nvim'
+alias vim='nvim'
+alias Vim='nvim'
+alias vIm='nvim'
+alias viM='nvim'
+alias VIm='nvim'
+alias ViM='nvim'
+alias vIM='nvim'
+alias VIM='nvim'
